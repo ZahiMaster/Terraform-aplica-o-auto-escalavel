784430d37d381477175781dd6eba3dd5ac9a3adf
ZahiMaster <zahi.master@outlook.com>
2020-12-14 23:06:17 -0300

    Criando VPC

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
-asd
+provider "aws" {
+    region = "${var.region}"  
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "${var.cidr_block}"
+
+    tags = {
+        name = "Terraform"
+    }  
+}
